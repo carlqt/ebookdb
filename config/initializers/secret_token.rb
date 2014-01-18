d553a8070e265f1ff74fd60c968094d96e333c67
carl <carlwilliam.tablante@gmail.com>
2014-01-18 06:13:45 -0500

    My first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebookdb::Application.config.secret_token = '41ccb2cbec310ca60556c13f1279a29f50c8f88f7afa3d36eed3997726b52d2ff464da6053d34df9d586e796712ee77e30b269be8d182b9dfa1217400a8dbcbb'
