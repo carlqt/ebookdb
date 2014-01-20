a5239beb80b032c4551175814ee4319cabdbbdcc
carl <carlwilliam.tablante@gmail.com>
2014-01-20 16:16:03 +0800

    Added a script to handle record create. Unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Book.destroy_all
+
+
+# Gets the proper file to load
+file_temp_path = File.dirname(__FILE__).to_s
+file_name = "bookfile.txt"
+
+file_path = file_temp_path[0, file_temp_path.length - 2]
+file_path = file_path + file_name
+
+File.open(file_path, "r").each_line do |line|
+  puts line.split(":")
+end
