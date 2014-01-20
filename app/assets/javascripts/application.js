6513388555b4de691dfd835d1d82f2f5e203cb1b
carl <carlwilliam.tablante@gmail.com>
2014-01-20 09:32:47 +0800

    Added a click event to right-menu button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+	$('.btn.menu-right').on("click", function() {
+		alert("I don't have a function yet");
+	});
+});
