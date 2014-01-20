2b89d4491949ea23b0cbbb5f7c0eb1c07e235186
carl <carlwilliam.tablante@gmail.com>
2014-01-20 09:36:43 +0800

    ran db migrate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id             :integer          not null, primary key
+#  title          :string(255)
+#  author         :string(255)
+#  genre          :string(255)
+#  description    :text
+#  related_series :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Book < ActiveRecord::Base
   attr_accessible :author, :description, :genre, :related_series, :title
 end
