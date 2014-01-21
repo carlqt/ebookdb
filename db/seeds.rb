6dd3ad2c24530e1af39c3bff7515dded5bf64dcb
carl <carlwilliam.tablante@gmail.com>
2014-01-20 22:19:24 -0500

    finsihed seed.rb and created a partial showbooks.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,7 @@ file_path = file_temp_path[0, file_temp_path.length - 2]
 file_path = file_path + file_name
 
 File.open(file_path, "r").each_line do |line|
-  puts line.split(":")
+  new_book = line.split(":")
+  Book.create(title: new_book[0].to_s.strip, author: new_book[1].to_s.strip, genre: new_book[2].to_s.strip, description: new_book[3].to_s.strip, related_series: new_book[4].to_s.strip)
+  # puts new_book[2].to_s.strip
 end
