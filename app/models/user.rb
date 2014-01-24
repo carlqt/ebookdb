26770be7d86ced8a46a3c0f06f60d46c32c42369
carl <carlwilliam.tablante@gmail.com>
2014-01-24 08:46:32 -0500

    Adding list dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
   has_many :lists
+  has_many :books, through: :lists
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   
